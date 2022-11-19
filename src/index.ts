0bca75fb7df20e62f2f0bcb31c6318b53bdc5291
Mark <m7mark@yandex.ru>
2022-11-19 23:18:39 +0300

    update bot.on trigger

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const ENVIRONMENT = process.env.NODE_ENV || '';
 const bot = new Telegraf(BOT_TOKEN);
 
 bot.command('about', about());
-bot.on('text', greeting());
+bot.on('message', greeting());
 
 //prod mode (Vercel)
 export const startVercel = async (req: VercelRequest, res: VercelResponse) => {
